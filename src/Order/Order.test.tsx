fa921a2b64d2ec8a6e12cae936b7d03ada895632
Ivan Gorshkov <ivanagorshkov160@gmail.com>
2021-09-30 21:23:48 +0300

    sem1
    :q

--- src/Order/Order.test.tsx
+++ src/Order/Order.test.tsx
@@ -1,7 +1,62 @@
 import React from 'react';
+import {OrderComponent} from './Order';
+import {shallow, configure, ReactWrapper} from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+jest.mock('../utils/getDate');
+import {getDate} from '../utils/getDate';
+
+
+configure({ adapter: new Adapter() });
 
 describe('Order.tsx', () => {
-	it('some test', () => {
-		// test something here
+
+	beforeEach(() => {
+		getDate.mockReturnValue('Чт, 30 сент.');
+	});
+
+	afterAll(() => {
+		jest.resetModules();
+	});
+
+	const data = {
+		order: {
+			id: 100,
+			date: 1588359900000,				
+			shop: 'Сбереги Мега Маркер',
+			items: []
+		}
+	};
+
+	const data2 = {
+		order: {
+			id: 100,
+			date: 1588359900000,				
+			shop: 'Сбереги Мега Маркер',
+			items: [
+				'Утиный пластмасса для показ новый год',
+				'Курица из нержавеющей стали, утка, гусь, голубь, питьевой фонтан',
+				'Новый стиль один розница яйцо для упаковки форма латекс',
+			]
+		}
+	};
+
+	const dataEmpty = { 
+		order: {} 
+	};
+
+	it('Пустой список', () => {
+		const wrapper = shallow(<OrderComponent {...data}/>);
+		expect(wrapper).toMatchSnapshot();
+	});
+
+	it('Список товаров', () => {
+		const wrapper = shallow(<OrderComponent {...data2}/>);
+		expect(wrapper).toMatchSnapshot();
+	});
+
+
+	it('Пустой Order', () => {
+		const wrapper = shallow(<OrderComponent {...dataEmpty}/>);
+		expect(wrapper).toMatchSnapshot();
 	});
 });
--- src/utils/sortOrders.test.ts
+++ src/utils/sortOrders.test.ts
@@ -1,18 +1,113 @@
-import {sortByItemCount} from './sortOrders';
+import { type } from 'os';
+import {getSortFunction, sortByDate, sortByItemCount, sortTypes, sortOrders} from './sortOrders';
 
 describe('sortByItemCount function', () => {
-	it('same items count', () => {
-		const order1 = {
-			items: ['item1', 'item2'],
-		};
+	it.each([
+		{ 
+			order1: { 
+				items: ['item1', 'item2'] 
+			},
+			order2: { 
+				items: ['1', '2'] 
+			},
+			res: 0
+		},
+		{ 
+			order1: { 
+				items: ['item1', 'item2'] 
+			}, 
+			order2: {
+				items: ['1']
+			},
+			res: 1
+		},
+		{ 
+			order1: { 
+				items: ['item1', 'item2']  
+			}, 
+			order2: { 
+				items: ['item1', 'item2','item3']  
+			},
+			res: -1
+		},
+		{	order1: { }, 
+			order2: {  },
+			res: 0
+		},
+	])('same items count', (data) => {
+		const result = sortByItemCount(data.order1, data.order2);
+		expect(result).toBe(data.res);
+	});
+});
 
-		const order2 = {
-			items: ['1', '2'],
-		};
+describe.each(
+	[
+		{
+			type: sortTypes.COUNT, 
+			res: sortByItemCount
+		},
+		{
+			type: sortTypes.DATE, 
+			res: sortByDate
+		},
+	]
+)('getSortFunction function', (data) => {
+	expect(data.res).toBe(getSortFunction(data.type));
+});
 
-		const result = sortByItemCount(order1, order2);
 
-		expect(result).toBe(0);
+describe('sortByDate function', () => {
+	it.each([
+		{ 
+			order1: { 
+				date: 1588359900000 
+			}, 
+			order2: { 
+				date: 1544356800000 
+			},
+			res: -1
+		},
+		{ 
+			order1: { 
+				date: 1544356800000 
+			}, 
+			order2: { 
+				date: 1588359900000
+			},
+			res: 1
+		},
+		{ 
+			order1: { 
+				date: 1588359900000 
+			}, 
+			order2: { 
+				date: 1588359900000
+			},
+			res: 0
+		},
+	])('same date', (data) => {
+		const result = sortByDate(data.order1, data.order2);
+		expect(result).toBe(data.res);
+	});
+});
+
+
+describe('sortOrders function', () => {
+	it('Вызов с пустыми order', () => {
+		const func = jest.fn();
+		const res = sortOrders([], func);
+		expect(res).toBeUndefined();
+	});
+
+	it('Вызов с пустой функцией', () => {
+		const res = sortOrders([{}, {}], undefined);
+		expect(res).toBeUndefined();
+	});
+
+	it('Вызов функции сортировки', () => {
+		const func = jest.fn();
+		sortOrders([{}, {}], func);
+		expect(func).toHaveBeenCalledTimes(1);
 	});
 });
 
