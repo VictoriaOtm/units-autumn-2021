a1bf92f7832e8b06711673344216e1fdb77e0ffd
Denis Vlasov <wd055@mail.ru>
2021-09-30 20:48:41 +0300

    Tmp

--- src/Order/Order.test.tsx
+++ src/Order/Order.test.tsx
@@ -1,7 +1,28 @@
 import React from 'react';
+import { OrderComponent } from './Order';
+import { shallow, configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import { Order } from '../data/fakeOrders';
+
+// jest.mock('../utils/getDate');
+// import { getDate } from '../utils/__mocks__/getDate';
+
+configure({ adapter: new Adapter() });
 
 describe('Order.tsx', () => {
+	const order: Order = {
+		items: ['1', '2'],
+		date: new Date().getMilliseconds()
+	};
+
+	// let wrapper = shallow(<OrderComponent order={order} />);
+
+	// beforeAll(() => {
+	// 	wrapper = shallow(<OrderComponent order={order} />);
+	// });
+
 	it('some test', () => {
-		// test something here
+		const wrapper = shallow(<OrderComponent order={order} />);
+		expect(wrapper).toMatchSnapshot();
 	});
 });
