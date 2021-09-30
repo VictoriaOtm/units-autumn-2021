8403685c995383d1a170e73e9c2671b875788941
Denis Kolosov <kdv.levart@gmail.com>
2021-09-30 21:40:22 +0300

    Initial version of unit-tests

--- src/Order/Order.test.tsx
+++ src/Order/Order.test.tsx
@@ -1,7 +1,44 @@
 import React from 'react';
 
+
+import Adapter from 'enzyme-adapter-react-16';
+import {shallow, configure} from 'enzyme';
+import {fakeOrders} from '../data/fakeOrders';
+import {OrderComponent} from './Order';
+
+jest.mock('../utils/getDate');
+import {getDate} from '../utils/getDate';
+
+
+configure({ adapter: new Adapter() });
+
 describe('Order.tsx', () => {
-	it('some test', () => {
-		// test something here
+	beforeEach(() => {
+		(getDate as jest.Mock).mockReturnValue('some string');
 	});
+
+	afterAll(() => {
+		jest.resetModules();
+	});
+
+	it ('no shop in order', () => {
+		const wrapper = shallow(<OrderComponent order={{date: 1}}/>);
+		expect(wrapper.getElement()).toBeNull();
+	});
+
+	it ('no date in order', () => {
+		const wrapper = shallow(<OrderComponent order={{shop: 'some shop'}}/>);
+		expect(wrapper.getElement()).toBeNull();
+	});
+
+	it('at least 1 items in order', () => {
+		const wrapper = shallow(<OrderComponent order={fakeOrders[1]}/>);
+		expect(wrapper).toMatchSnapshot();
+	});
+
+	it('no items', () => {
+		const wrapper = shallow(<OrderComponent order={fakeOrders[0]}/>);
+		expect(wrapper).toMatchSnapshot();
+	});
+
 });
--- src/utils/sortOrders.test.ts
+++ src/utils/sortOrders.test.ts
@@ -1,18 +1,137 @@
-import {sortByItemCount} from './sortOrders';
+import {sortByItemCount, sortByDate, getSortFunction, sortOrders, sortTypes} from './sortOrders';
+import {fakeOrders} from '../data/fakeOrders';
 
-describe('sortByItemCount function', () => {
-	it('same items count', () => {
-		const order1 = {
-			items: ['item1', 'item2'],
+
+
+describe('sortByDate function', () => {
+	it('same date', () => {
+		const firstOrder = {
+			date: 1,
 		};
 
-		const order2 = {
-			items: ['1', '2'],
+		const secondOrder = {
+			date: 1,
 		};
 
-		const result = sortByItemCount(order1, order2);
+		expect(sortByDate(firstOrder, secondOrder)).toBe(0);
+	});
 
-		expect(result).toBe(0);
+	it('first orders date is bigger, than second one', () => {
+		const firstOrder = {
+			date: 2,
+		};
+
+		const secondOrder = {
+			date: firstOrder.date - 1,
+		};
+
+		expect(sortByDate(firstOrder, secondOrder)).toBe(-1);
+	});
+
+	it('first orders date is less, than second one', () => {
+		const secondOrder = {
+			date: 2,
+		};
+
+		const firstOrder = {
+			date: secondOrder.date - 1,
+		};
+
+		expect(sortByDate(firstOrder, secondOrder)).toBe(1);
+	});
+
+	it('no "date" field in one of orders', () => {
+		const secondOrder = {
+			date: 2,
+		};
+
+		const firstOrder = {
+			items: ['firstItem'],
+		};
+
+		expect(sortByDate(firstOrder, secondOrder)).toBe(0);
 	});
 });
 
+describe('sortByItemCount function', () => {
+	it('same amount of items', () => {
+		const firstOrder = {
+			items: ['firstItem', 'secondItem', 'thirdItem'],
+		};
+
+		const secondOrder = {
+			items: [...firstOrder.items]
+		};
+
+		expect(sortByItemCount(firstOrder, secondOrder)).toBe(0);
+	});
+
+	it('first order has more items, than second', () => {
+		const firstOrder = {
+			items: ['firstItem', 'secondItem', 'thirdItem'],
+		};
+
+		const secondOrder = {
+			items: firstOrder.items.slice(0, firstOrder.items.length - 1),
+		};
+		expect(sortByItemCount(firstOrder, secondOrder)).toBe(1);
+	});
+
+	it('second order has more items, than first', () => {
+		const secondOrder = {
+			items: ['firstItem', 'secondItem', 'thirdItem'],
+		};
+
+		const firstOrder = {
+			items: secondOrder.items.slice(0, secondOrder.items.length - 1),
+		};
+
+		expect(sortByItemCount(firstOrder, secondOrder)).toBe(-1);
+	});
+
+	it('no "items" field in one of orders', () => {
+		const secondOrder = {
+			items: ['firstItem', 'secondItem', 'thirdItem'],
+		};
+
+		const firstOrder = {
+			date: 1,
+		};
+
+		expect(sortByItemCount(firstOrder, secondOrder)).toBe(0);
+	});
+});
+
+describe('getSortFunction function', () => {
+	it('DATE sort type', () => {
+		expect(getSortFunction(sortTypes.DATE)).toBe(sortByDate);
+	});
+
+	it('COUNT sort type', () => {
+		expect(getSortFunction(sortTypes.COUNT)).toBe(sortByItemCount);
+	});
+});
+
+describe('sortOrders function', () => {
+	it('empty orders array', () => {
+		expect(sortOrders([], sortByDate)).toBe(undefined);
+	});
+
+	it('null instead of function', () => {
+		expect(sortOrders([{date: 1}], null)).toBe(undefined);
+	});
+
+	it('sort by date', () => {
+		const sortedOrders = [fakeOrders[2], fakeOrders[0], fakeOrders[1]];
+		const ordersToSort = fakeOrders.slice(0, 3);
+		sortOrders(ordersToSort, sortByDate);
+		expect(ordersToSort).toEqual(sortedOrders);
+	});
+
+	it('sort by itemsCount', () => {
+		const sortedOrders = [fakeOrders[0], fakeOrders[3], fakeOrders[1], fakeOrders[2]];
+		const ordersToSort = fakeOrders.slice(0, 4);
+		sortOrders(ordersToSort, sortByItemCount);
+		expect(ordersToSort).toEqual(sortedOrders);
+	});
+});
