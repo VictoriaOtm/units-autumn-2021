14184a1338b7bd42f2d1ac604f8058643549aa37
Denis Vlasov <wd055@mail.ru>
2021-09-30 22:08:13 +0300

    Final

--- src/Order/Order.test.tsx
+++ src/Order/Order.test.tsx
@@ -4,24 +4,67 @@ import { shallow, configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import { Order } from '../data/fakeOrders';
 
-// jest.mock('../utils/getDate');
-// import { getDate } from '../utils/__mocks__/getDate';
+jest.mock('../utils/getDate');
+import { getDate } from '../utils/__mocks__/getDate';
 
 configure({ adapter: new Adapter() });
 
 describe('Order.tsx', () => {
+	const date = new Date('2021-09-30').getTime();
 	const order: Order = {
-		items: ['1', '2'],
-		date: new Date().getMilliseconds()
+		items: ['Заказ 1', 'Заказ 2'],
+		date: date,
+		shop: 'shop'
 	};
 
-	// let wrapper = shallow(<OrderComponent order={order} />);
-
-	// beforeAll(() => {
-	// 	wrapper = shallow(<OrderComponent order={order} />);
-	// });
-
 	it('some test', () => {
+		const wrapper = shallow(<OrderComponent order={order} />);
+		expect(wrapper).toMatchSnapshot();
+	});
+
+	test.each([
+		{
+			order: null
+		},
+		{
+			order: {
+				items: ['1', '2'],
+				date: date
+			}
+		},
+		{
+			order: {
+				items: ['1', '2'],
+				shop: 'shop'
+			}
+		}
+	])('null', ({ order }) => {
+		const wrapper = shallow(<OrderComponent order={order} />);
+		expect(wrapper).toEqual({});
+	});
+
+	test.each([
+		{
+			order: {
+				items: [],
+				date: date,
+				shop: 'shop'
+			}
+		},
+		{
+			order: {
+				items: null,
+				date: date,
+				shop: 'shop'
+			}
+		},
+		{
+			order: {
+				date: date,
+				shop: 'shop'
+			}
+		}
+	])('not items', ({ order }) => {
 		const wrapper = shallow(<OrderComponent order={order} />);
 		expect(wrapper).toMatchSnapshot();
 	});
